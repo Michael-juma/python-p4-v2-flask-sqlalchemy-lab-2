f25129197a7aee2fee9bcc5acfcd10f68e4a362e
Juma Michael <obudho76@gmail.com>
2025-06-26 15:06:24 +0300

    initial commits

--- server/app.py
+++ server/app.py
@@ -1,21 +1,23 @@
 from flask import Flask
 from flask_migrate import Migrate
-
 from models import db
 
+# Initialize app
 app = Flask(__name__)
+
+# Configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Bind SQLAlchemy and Migrate to app
+db.init_app(app)
 migrate = Migrate(app, db)
 
-db.init_app(app)
-
-
+# Basic route for testing
 @app.route('/')
 def index():
     return '<h1>Flask SQLAlchemy Lab 2</h1>'
 
-
+# Entry point
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
